82bce88151813b1294f4b966477a143691ac69a0
Waqas7281 <you@example.com>
2025-10-19 12:49:49 +0500

    change in frontend

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,10 +6,7 @@ const authenticateToken = require("../middlewares/auth.middleware.js");
 userRoute.get("/user-data", authenticateToken, getCurrentUser);
 
 
-// Protected route to test token expiration
-userRoute.get("/current", authenticateToken, (req, res) => {
-    res.json({ message: 'Profile accessed', user: req.user });
-});
+
 
 module.exports = userRoute;
 
